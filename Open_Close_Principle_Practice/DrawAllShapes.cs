5aed28cf46470303cba1c820242a287417d3bd8e
messboy home <messboy000@gmail.com>
2014-09-27 22:52:13 +0800

    新需求 : 加入特定形狀先排序－仍須符合OCP

--- Open_Close_Principle_Practice/DrawAllShapes.cs
+++ Open_Close_Principle_Practice/DrawAllShapes.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,8 +12,12 @@ namespace Open_Close_Principle_Practice
         /// 繪出全部圖形
         /// </summary>
         /// <param name="shapes">圖形集合</param>
-        public static void DrawAll(IList<Shape> shapes)
+        public static void DrawAll(ArrayList shapes)
         {
+            //依照特定圖形排序
+            shapes.Sort(new ShapeComparer());
+
+            //畫圖
             foreach (Shape shape in shapes)
             {
                 shape.Draw();
--- Open_Close_Principle_Practice/Program.cs
+++ Open_Close_Principle_Practice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,11 +13,17 @@ namespace Open_Close_Principle_Practice
             Shape s1 = new Square();
             Shape c1 = new Circle();
 
-            List<Shape> shapes = new List<Shape>();
+            ArrayList shapes = new ArrayList();
             shapes.Add(s1);
             shapes.Add(c1);
             shapes.Add(c1);
             shapes.Add(c1);
+
+            /*
+             *新需求 提供先繪畫圓形的功能
+             *如此，open for extension close for modification 就會被破壞
+             *允許被打一槍，但之後就要再遵守
+             */
 
             DrawAllShapes.DrawAll(shapes);
         }
